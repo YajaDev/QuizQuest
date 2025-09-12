3dc472a7d0e10988830bc0d1552d3b0e38b407aa
YajaDev <ajay.tomagan.10212002@gmail.com>
2025-09-12 10:58:12 +0800

    Add other links to sidenav and Change variable in theme

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,43 +4,29 @@ const Sidebar = ({isOpen}) => {
   
   return (
     <>
-      <ul className={`${isOpen ?'':'hidden'} h-[cal(100% - 1px)] w-50 p-3 mt-[1px] bg-[var(--bg-white)] z-10`}>
-        <NavLink to={'/'} className="flex items-center gap-5 bg-[var(--bg-grey)] px-2 rounded-full cursor-pointer">
+      <ul className={`${isOpen ?'':'hidden'} flex flex-col gap-3 h-[cal(100% - 1px)] w-50 p-3 mt-[1px] bg-white z-10`}>
+        <NavLink to={'/'} className="flex items-center gap-5 bg-gray px-2 rounded-full cursor-pointer">
           <i className=" bx bx-home-alt bx-sm p-3"></i>
           <span className="text-l font-medium">Home</span>
         </NavLink>
+        <NavLink to={'quiz'} className="flex items-center gap-5 hover:bg-gray px-2 rounded-full cursor-pointer">
+          <i className="bx bx-book bx-sm p-3"></i>
+          <span className="text-l font-medium">Quiz</span>
+        </NavLink>
+        <NavLink to={'module'} className="flex items-center gap-5 hover:bg-gray px-2 rounded-full cursor-pointer">
+          <i className="bx bxs-graduation bx-sm p-3"></i>
+          <span className="text-l font-medium">Module</span>
+        </NavLink>
+        <NavLink to={'members'} className="flex items-center gap-5 hover:bg-gray px-2 rounded-full cursor-pointer">
+          <i className="bx bx-group bx-sm p-3"></i>
+          <span className="text-l font-medium">Members</span>
+        </NavLink>
       </ul>
-      <div className="w-10 h-10 mt-[1px] bg-[var(--bg-white)] z-10">
-        <div className="w-10 h-10 rounded-tl-full bg-[var(--bg-grey)]" />
+      <div className="w-10 h-10 mt-[1px] bg-white z-10">
+        <div className="w-10 h-10 rounded-tl-full bg-gray" />
       </div>
     </>
   )
 }
-    // <ul className="">
-      
-    //   <li>
-    //     <a className="home active" href="./index.html">
-    //       <i className=" bx bx-home-alt"></i>
-    //       <span>Home</span>
-    //     </a>
-    //   </li>
-    //   <li>
-    //     <a className="quiz" href="./quiz.html">
-    //       <i className="bx bx-book"></i>
-    //       <span>Quiz</span>
-    //     </a>
-    //   </li>
-    //   <li>
-    //     <a className="module" href="./course.html">
-    //       <i className="bx bxs-graduation"></i>
-    //       <span>Module</span>
-    //     </a>
-    //   </li>
-    //   <li>
-    //     <a className="member" href="./member.html">
-    //       <i className="bx bx-group"></i>
-    //       <span>Members</span>
-    //     </a>
-    //   </li>
-    // </ul>
+
 export default Sidebar
