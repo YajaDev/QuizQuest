80233287729d9aa93a781987e846f8b2c40bf03a
YajaDev <ajay.tomagan.10212002@gmail.com>
2025-09-12 16:53:14 +0800

    Fix sidebar hover effect

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,13 +5,13 @@ const Navbar = ({setIsOpen, isOpen}) => {
     
   
   return ( 
-    <nav className='px-3 py-3 bg-[var(--bg-white)] z-10'>
+    <nav className='px-3 py-3 bg-white z-10'>
       <div className="flex items-center gap-3 ">
         <i 
         onClick={() => setIsOpen(!isOpen)}
         className="bx bx-menu-alt-left bx-sm 
-          p-1.5 cursor-pointer rounded-md transition-colors duration-400
-        hover:bg-[var(--bg-grey)]"/>
+          p-1.5 cursor-pointer rounded-md transition-colors duration-500
+        hover:bg-gray"/>
         <Link to={"/"}>
           <h1 className='font-sans text-2xl font-bold tracking-wide cursor-pointer'>QuizQuest</h1>
         </Link>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -5,19 +5,23 @@ const Sidebar = ({isOpen}) => {
   return (
     <>
       <ul className={`${isOpen ?'':'hidden'} flex flex-col gap-3 h-[cal(100% - 1px)] w-50 p-3 mt-[1px] bg-white z-10`}>
-        <NavLink to={'/'} className="flex items-center gap-5 bg-gray px-2 rounded-full cursor-pointer">
+        <NavLink to={'/'} className="flex items-center gap-5 px-2 rounded-full cursor-pointer transition-colors duration-250
+        hover:bg-gray">
           <i className=" bx bx-home-alt bx-sm p-3"></i>
           <span className="text-l font-medium">Home</span>
         </NavLink>
-        <NavLink to={'quiz'} className="flex items-center gap-5 hover:bg-gray px-2 rounded-full cursor-pointer">
+        <NavLink to={'quiz'} className="flex items-center gap-5 px-2 rounded-full cursor-pointer transition-colors duration-250
+        hover:bg-gray">
           <i className="bx bx-book bx-sm p-3"></i>
           <span className="text-l font-medium">Quiz</span>
         </NavLink>
-        <NavLink to={'module'} className="flex items-center gap-5 hover:bg-gray px-2 rounded-full cursor-pointer">
+        <NavLink to={'module'} className="flex items-center gap-5 px-2 rounded-full cursor-pointer transition-colors duration-250
+        hover:bg-gray">
           <i className="bx bxs-graduation bx-sm p-3"></i>
           <span className="text-l font-medium">Module</span>
         </NavLink>
-        <NavLink to={'members'} className="flex items-center gap-5 hover:bg-gray px-2 rounded-full cursor-pointer">
+        <NavLink to={'members'} className="flex items-center gap-5 px-2 rounded-full cursor-pointer transition-colors duration-250
+        hover:bg-gray">
           <i className="bx bx-group bx-sm p-3"></i>
           <span className="text-l font-medium">Members</span>
         </NavLink>
